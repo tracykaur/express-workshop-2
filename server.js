5bf185b7a1dd837550001bdf3e0c3de1d0ab33e0
tracykaur <tracykaur@yahoo.com>
2017-11-21 23:15:50 +1000

    formidable

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var app = express();
+app.use(express.static("public"));
+var fs = require('fs');
+var formidable = require('express-formidable');
 
-app.get("/hello", function (request, response) {
-  response.send("Hello World!");
+
+
+app.get('/get-posts', function(request, response){
+  fs.readFile(__dirname+'/data/posts.json', function(error, data){
+    if(error){
+      console.log('Error reading posts.json: '+error);
+      response.status(500);
+      response.send(error);
+    } else {
+      response.send(data.toString());
+    }
+  });
 });
+
+app.post('/create-post', function(request, response){
+
+});
+
 app.listen(3000, function () {
   console.log('Server has started listening on port 3000. ');
 });
