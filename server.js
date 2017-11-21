7393f2b684ed6927bdf4b3d8ebba45cc08258bd0
tracykaur <tracykaur@yahoo.com>
2017-11-22 00:07:47 +1000

    get read post

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require('express');
-var app = express();
-app.use(express.static("public"));
-var fs = require('fs');
 var formidable = require('express-formidable');
+var fs = require('fs');
 
+var app = express();
 
+app.use(express.static('public'));
+app.use(formidable());
+
+app.post('/create-post', function(request, response){
+  var now = Date.now();
+  var newPost = {
+    timestamp: now,
+    content: request.fields.blogpost
+  }
+
+  fs.readFile(__dirname+'/data/posts.json', function(error, data){
+    if(error){
+      console.log('Error reading posts.json: '+error);
+      response.status(500);
+      response.send(error);
+    } else {
+      var posts = JSON.parse(data);
+      posts.blogposts.push(newPost);
+      console.log(posts.blogposts)
+
+      response.send(newPost);
+    }
+  });
+});
 
 app.get('/get-posts', function(request, response){
   fs.readFile(__dirname+'/data/posts.json', function(error, data){
@@ -13,13 +36,9 @@ app.get('/get-posts', function(request, response){
       response.status(500);
       response.send(error);
     } else {
-      response.send(data.toString());
+      response.send(data);
     }
   });
-});
-
-app.post('/create-post', function(request, response){
-
 });
 
 app.listen(3000, function () {
